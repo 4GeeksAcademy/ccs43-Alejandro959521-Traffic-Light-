5065917e70dc11a10d000ae34a5286e853737600
Miguel Ojeda <113554869+Alejandro959521@users.noreply.github.com>
2023-06-30 03:03:51 +0000

    Ejercicio completo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,88 @@
-import React from "react";
-
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
+import React, { useState } from "react";
 //create your first component
 const Home = () => {
+
+	const [color, setColor] = useState("gray")
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<div className="container ">
+			<div className="  d-flex justify-content-center align-items-center" >
+				<div className="  " style={{ height: "200px", width: "30px", backgroundColor: "rgba(10, 0, 5, 0.93)" }} >
+				</div>
+			</div>
+			<div className="d-flex justify-content-center align-items-center" >
+
+				<div className=" " style={{
+					height: "300px", width: "150px", backgroundColor: "rgba(10, 0, 5, 0.93)",
+					borderRadius: "50px 50px 50px 50px"
+				}}
+
+				>
+
+					<div className="d-flex justify-content-center align-items-center">
+						<div className="rounded-circle mt-2" style={{ height: "90px", width: "90px", backgroundColor: (color === "red" ? "red" : "gray") }}
+
+							onClick={() => {
+
+								setColor((prev) => {
+
+									if (prev != "red") return "red"
+									return "gray"
+
+								}
+								);
+
+							}
+
+
+
+							}
+
+						></div>
+					</div>
+					<div className="d-flex justify-content-center align-items-center">
+						<div className="rounded-circle mt-2" style={{
+							height: "90px", width: "90px", backgroundColor: (color === "yellow" ? "yellow" : "gray")
+
+						}} onClick={() => {
+
+							setColor((prev) => {
+
+								if (prev != "yellow") return "yellow"
+								return "gray"
+
+							}
+							);
+
+						}}></div>
+					</div>
+					<div className="d-flex justify-content-center align-items-center">
+						<div className="rounded-circle mt-2" style={{
+							height: "90px", width: "90px",
+							backgroundColor: (color === "green" ? "green" : "gray")
+						}}
+
+							onClick={() => {
+
+								setColor((prev) => {
+
+									if (prev != "green") return "green"
+									return "gray"
+
+								}
+								);
+
+
+
+							}}>
+
+						</div>
+
+					</div>
+				</div>
+			</div>
+		</div >
 	);
 };
 
